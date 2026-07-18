7a1c323858f756c379c26b7cc22af3a141544327
YukkuriC <799433638@qq.com>
2026-07-18 15:38:34 +0800

    real immutable nested modify

--- common/src/main/java/io/yukkuric/hexflow/actions/OpNestedModify.kt
+++ common/src/main/java/io/yukkuric/hexflow/actions/OpNestedModify.kt
@@ -8,8 +8,8 @@ import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.iota.ListIota
 import at.petrak.hexcasting.api.casting.mishaps.Mishap
 import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
-import io.yukkuric.hexflow.helpers.deepCopy
-import io.yukkuric.hexflow.mixin_interface.MutableSpellList
+import at.petrak.hexcasting.api.utils.TreeList
+import kotlin.math.roundToInt
 
 // ([114,514,[1,[[9[19]],810]]],[2,1,0,1,0],get_caster)splat,hexflow:nested_modify
 // ([114],[2,1,0,1,0],get_caster)splat,hexflow:nested_modify
@@ -17,28 +17,37 @@ object OpNestedModify : ConstMediaAction {
     override val argc = 3
 
     override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
-        args.getList(0)
-        val orig = args[0] as ListIota
-        val listNbt = orig.deepCopy(env.world.registryAccess())
+        val theList = args.getList(0)
         val idxList = args.getList(1)
-        val n = idxList.size()
-        var setter = listNbt.list
         try {
-            for (i in 0 until n) {
-                var idx_iota = idxList.getAt(i)
-                if (idx_iota !is DoubleIota) throw MishapInvalidIota.ofType(idx_iota, i, "double")
-                var idx = Math.round(idx_iota.double).toInt()
-                val size = setter.size()
-                if (idx < 0) idx += setter.size()
-                if (idx >= size) return listOf(args[0])
-                if (i == n - 1) (setter as MutableSpellList)[idx] = args[2]
-                else setter = (setter.getAt(idx) as ListIota).list
-            }
+            val modified = theRealNestedModify(theList, idxList, args[2], 0)
+                ?: return listOf(args[0])
+            return listOf(ListIota(modified))
         } catch (e: Mishap) {
             throw e
         } catch (e: Throwable) {
             return listOf(args[0])
         }
-        return listOf(listNbt)
+    }
+
+    // return null for not modified
+    fun theRealNestedModify(theList: List<Iota>, idxList: List<Iota>, setData: Iota, depth: Int): TreeList<Iota>? {
+        val idxIota = idxList[depth]
+        if (idxIota !is DoubleIota) throw MishapInvalidIota.ofType(idxIota, depth, "double")
+        var idx = idxIota.double.roundToInt()
+        val setter = theList.toMutableList()
+        val size = setter.size
+        if (idx < 0) idx += size
+        if (idx >= size) return null
+        if (depth == idxList.size - 1) {
+            setter[idx] = setData
+        } else {
+            val innerIota = setter[idx]
+            if (innerIota !is ListIota) throw MishapInvalidIota.ofType(innerIota, depth, "list")
+            val inner = theRealNestedModify(innerIota.list, idxList, setData, depth + 1)
+                ?: return null
+            setter[idx] = ListIota(inner)
+        }
+        return TreeList.from(setter)
     }
 }
--- common/src/main/java/io/yukkuric/hexflow/mixin_interface/MutableSpellList.java
+++
@@ -1,7 +0,0 @@
-package io.yukkuric.hexflow.mixin_interface;
-
-import at.petrak.hexcasting.api.casting.iota.Iota;
-
-public interface MutableSpellList {
-    void set(int idx, Iota data);
-}
