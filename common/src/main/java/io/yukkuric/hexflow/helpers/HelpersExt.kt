f4ad1d82db6fec23b794723cb418db7d4eecc403
YukkuriC <799433638@qq.com>
2026-09-22 23:09:29 +0800

    common nested iota list dump

--- common/src/main/java/io/yukkuric/hexflow/helpers/HelpersExt.kt
+++ common/src/main/java/io/yukkuric/hexflow/helpers/HelpersExt.kt
@@ -1,7 +1,14 @@
 package io.yukkuric.hexflow.helpers
 
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.utils.serializeToNBT
+import net.minecraft.nbt.ListTag
 import net.minecraft.util.Mth
 import net.minecraft.world.phys.Vec3
 
 val Vec3.attachCenter
     get() = Vec3(0.5 + Mth.floor(x), 0.5 + Mth.floor(y), 0.5 + Mth.floor(z))
+
+fun Iterable<Iterable<Iota>>.serializeToNBT() = ListTag().also {
+    for (s in this) it.add(s.serializeToNBT())
+}
